bcb64bd188b777ba4a775cb65df296e36e6244f7
Luis Forero <foluis@hotmail.com>
2020-03-26 22:42:59 -0600

    Vamos

---
+++ Code/NA.SimpleSurveyInvitation/NA.SimpleSurveyInvitation.Web/Pages/Survey.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace NA.SimpleSurveyInvitation.Web.Pages
+{
+    public partial class Survey
+    {
+        //public void GoHome()
+        //{
+        //    UriHelper.NavigateTo("/");
+        //}
+    }
+}
